048851aebbd5ce1cb2ba09a559c759b2ac32895f
nutec <zsolt.schumacher@gmail.com>
2025-07-23 11:23:05 +0200

    Added dashboard header

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  template: `
-    <h1>{{ title }}</h1>
-    <p>Welcome to your dashboard! Only logged-in users can see this page.</p>
-  `,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   private authService = inject(AuthService);
@@ -15,6 +14,14 @@ export class DashboardComponent {
 
   constructor() {
     this.updateTitleBasedOnRole(); // Update the title dynamically
+  }
+
+  get username(): string | null {
+    return this.authService.getLoggedInUsername();
+  }
+
+  get authRole(): string | null {
+    return this.authService.getLoggedInRole();
   }
 
   // Method to update the title based on the user's role
